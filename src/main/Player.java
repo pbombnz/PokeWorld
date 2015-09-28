5045ba8fcc28b578ae245c73a98250523648d9eb
sushantbalajee <sushantbalajee@10.140.47.11>
2015-09-28 13:41:56 +1300

    -started gametask class -added to the player class

--- src/main/Player.java
+++ src/main/Player.java
@@ -2,19 +2,65 @@ package main;
 
 import gameObjects.Item;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
+
 	public static int HEALTH = 100;
-	
+	public static int ATTACK = 10;
+
+	private final int id;
+	private final int maxItems = 6;
+
+	private int health;
+	private int attack;
 	private String name;
-	private int id;
-	
-	private int totalHealth;
-	private int healthLeft;
-	
-	private List<Item> inventory;
-	
-	//private int attack;
+
+	private List<Item> inventory = new ArrayList<Item>();
+
 	Location location;
+
+	public Player(int ID, int attack, int health, List<Item> items){
+		this.id = ID;
+		this.attack = attack;
+		this.health = health;
+		if(items != null){
+			this.inventory.addAll(items);
+		}
+	}
+
+	public int getHealth(){
+		return health;
+	}
+
+	public void updateHealth(int change){
+		health += change;
+		if(health < 0){
+			health = 0;
+		}
+	}
+
+	public boolean isDead(){
+		return health <= 0;
+	}
+
+	public int getAttack(){
+		return attack;
+	}
+
+	public void updateAttack(int change){
+		this.attack += change;
+	}
+
+	public List<Item> getInventory(){
+		return inventory;
+	}
+
+	public boolean addToInventory(Item item){
+		if (item == null){
+			return false;
+		}
+		return inventory.add(item);
+	}
 }
--- src/main/Room.java
+++ src/main/Room.java
@@ -2,4 +2,11 @@ package main;
 
 public class Room {
 	Board board;
+	public Room(){
+		
+	}
+	public Room importRoom(String fileName){
+		return new Room();
+		
+	}
 }
