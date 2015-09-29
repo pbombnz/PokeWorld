719867646c769b1becae18ddecf06a29c80b7d5b
Prashant B <prashant_bhikhu@hotmail.com>
2015-09-29 23:08:33 +1300

    Made Location and Position one class. Contained the same thing anyways

--- src/game/Location.java
+++ src/game/Location.java
@@ -44,4 +44,37 @@ public class Location {
 	public void moveWest() {
 		this.x = this.x - 1;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((room == null) ? 0 : room.hashCode());
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		if (room == null) {
+			if (other.room != null)
+				return false;
+		} else if (!room.equals(other.room))
+			return false;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+	
+
 }
