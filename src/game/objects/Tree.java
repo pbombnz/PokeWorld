0f3292ad48c7506b10a7931224868c65e82cf4b9
Donald Tang <tangyfruits@gmail.com>
2015-10-01 23:46:10 +1300

    Added new Character Xerneas with Sprites - Donald

--- src/game/objects/Tree.java
+++ src/game/objects/Tree.java
@@ -4,6 +4,11 @@ import javax.swing.ImageIcon;
 
 public class Tree implements GameObject {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4087421334363366222L;
+
 	@Override
 	public ImageIcon getSpriteImage() {
 		return GameObject.TREE;
