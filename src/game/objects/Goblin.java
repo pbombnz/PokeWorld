37c89d7ba482bf3e12063442b1a9fba4291792b5
sushantbalajee <sushantbalajee@10.140.98.77>
2015-10-06 12:56:31 +1300

    added getters and setter for monster health and attack and also and isDead method for the monster

--- src/game/objects/Goblin.java
+++ src/game/objects/Goblin.java
@@ -2,13 +2,22 @@ package game.objects;
 
 import javax.swing.ImageIcon;
 
+
 import game.Location;
 
 /**
  *@author Wang Zhen
  */
 public class Goblin implements Monster{
+	
+	public int health;
+	public int attack;
 
+	public Goblin(int attack, int health){
+		this.attack = attack;
+		this.health = health;
+	}
+	
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
@@ -18,15 +27,26 @@ public class Goblin implements Monster{
 	@Override
 	public int attack() {
 		// TODO Auto-generated method stub
-		return 10;
+		return attack;
 	}
 
 	@Override
-	public int health() {
+	public int getHealth() {
 		// TODO Auto-generated method stub
-		return 50;
+		return health;
+	}
+	
+	public void setHealth(int change){
+		health = change;
+		if(health < 0){
+			health = 0;
+		}
 	}
 
+	public boolean isDead(){
+		return health <= 0;
+	}
+	
 	@Override
 	public ImageIcon getSpriteImage() {
 		// TODO Auto-generated method stub
