7a9a9fe2f978c835016ef594c0f92f8d6931ee54
Prashant B <prashant_bhikhu@hotmail.com>
2015-10-05 19:28:00 +1300

    Added More packet classes and added comments to the overall class so it makes sense for others to read.

--- src/network/Packets.java
+++ src/network/Packets.java
@@ -12,12 +12,33 @@ import game.Player;
 *
 */
 public class Packets {
-	// Client -> Server
+	/**
+	 * USED: Client -> Server
+	 *
+	 * A packet containing the new player with the avatar and set name is
+	 * sent to the server to be added the global game object within the server.
+	 */
 	public static class NewPlayer {
 		Player player;
 	}
+	
+	/** 
+	 * USED: Server -> Client
+	 * 
+	 * Sends an empty packet to the client to understand that the name is already
+	 * in use (or invalid is some other way that conflicts within the server).
+	 */
+	public static class NewPlayer_Error_NameAlreadyInUse {
+	}
 
-	// Server -> Client
+	/**
+	 * USED: Server -> Client
+	 * 
+	 * A packet that sends over the game object from the server to
+	 * a client when the client has already initalized a connection
+	 * and chosen the player name and avatar.
+	 *
+	 */
 	public static class NewGame {
 		Game game;
 	}
