e3a6b48451133a491584b75d33ddb69dba37fd74
Prashant B <prashant_bhikhu@hotmail.com>
2015-10-06 23:10:51 +1300

    Handle Unexpected Inputs in the ServerFrame better. Added disconnect() method in GameServer to properly handle the server ending although not finished

--- src/ui/ServerFrame.java
+++ src/ui/ServerFrame.java
@@ -1,19 +1,18 @@
 package ui;
 
-import game.Game;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
-import javax.management.RuntimeErrorException;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JTextArea;
+import javax.swing.text.DefaultCaret;
 
 import network.GameServer;
 
@@ -23,7 +22,13 @@ public class ServerFrame extends JFrame implements ActionListener {
 	private static final int FRAME_WIDTH = 600;
 	private static final int FRAME_HEIGHT = 300;
 	
-	private JTextArea console;
+	private JMenuBar menuBar = new JMenuBar();
+	JMenu gameMenu = new JMenu("Server");
+	JMenuItem connect = new JMenuItem("Connect");
+	JMenuItem disconnect = new JMenuItem("Disconnect");
+	JMenuItem exit = new JMenuItem("Exit");
+	
+	private JTextArea console = new JTextArea();
 	private JScrollPane consoleScrollPane;
 	
 	private GameServer gameServer;
@@ -34,14 +39,7 @@ public class ServerFrame extends JFrame implements ActionListener {
 		setResizable(false);
 
 		// Create Menu
-		JMenuBar menuBar = new JMenuBar();
-		JMenu gameMenu = new JMenu("Server");
-		
 		menuBar.add(gameMenu);
-
-		JMenuItem connect = new JMenuItem("Connect");
-		JMenuItem disconnect = new JMenuItem("Disconnect");
-		JMenuItem exit = new JMenuItem("Exit");
 
 		gameMenu.add(connect);
 		gameMenu.add(disconnect);
@@ -52,22 +50,24 @@ public class ServerFrame extends JFrame implements ActionListener {
 		disconnect.addActionListener(this);
 		exit.addActionListener(this);
 		
-		// Create Text Area for Console
-		console = new JTextArea();
+		// Sets the Console properties so that it cannot be editted (only for display), and auto scrolls
 		console.setEditable(false);
+		((DefaultCaret) console.getCaret()).setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+		// Wrap the console in a scrollpane to enable scrolling
+		consoleScrollPane = new JScrollPane(console);
 
-
+		// Add the menu and console scrollpane to the frame
 		setJMenuBar(menuBar);
-		
-		JScrollPane consoleScrollPane = new JScrollPane(console);
-
 		add(consoleScrollPane);
+		// Finally show frame
 		setVisible(true);
 		
 		try {
 			gameServer = new GameServer(this);
+			connect.setEnabled(false);
+			disconnect.setEnabled(true);
 		} catch (IOException e) {
-			throw new RuntimeException(e);
+			JOptionPane.showInternalMessageDialog(this, "Cannot connect", "Server Error", JOptionPane.ERROR_MESSAGE);
 		}
 		
 	}
@@ -81,12 +81,17 @@ public class ServerFrame extends JFrame implements ActionListener {
 			// Load Game Map 1 for now (NO USER SELECTION)
 			try {
 				gameServer = new GameServer(this);
+				connect.setEnabled(false);
+				disconnect.setEnabled(true);
 			} catch (IOException e) {
-				throw new RuntimeException(e);
+				JOptionPane.showInternalMessageDialog(this, e.getMessage(), "Server Error", JOptionPane.ERROR_MESSAGE);
 			}
 		} else if(menuItem.getText().equals("Disconnect")) {
-			System.exit(0);
+			connect.setEnabled(true);
+			disconnect.setEnabled(false);
+			gameServer.disconnect();
 		} else if(menuItem.getText().equals("Exit")) {
+			gameServer.disconnect();
 			System.exit(0);
 		}
 	}
