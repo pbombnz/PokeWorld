2e763b403fd4aeb37b47c6b016c5d13d23bf407c
Prashant B <prashant_bhikhu@hotmail.com>
2015-10-10 17:11:21 +1300

    Added Comments in Direction Enum file

--- src/game/Direction.java
+++ src/game/Direction.java
@@ -1,5 +1,12 @@
 package game;
 
+/**
+ * This class determines the way players and certain objects are facing
+ * and which particular sprite to draw when the direction is changed.
+ * 
+ * @author Prashant
+ *
+ */
 public enum Direction {
 	 FACE_LEFT, FACE_RIGHT, BACK_LEFT, BACK_RIGHT;
 }
