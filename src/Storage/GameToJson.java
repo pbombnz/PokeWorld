c348aa94b190cfa972a8006ab9449454a0a55486
Prashant B <prashant_bhikhu@hotmail.com>
2015-10-12 00:23:19 +1300

    Added Backup Saving and loading methods just incase Pri doesnt do it.

--- src/Storage/GameToJson.java
+++ src/Storage/GameToJson.java
@@ -1,13 +1,22 @@
 package Storage;
 
 import java.util.List;
+import java.awt.FileDialog;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
+
+import javax.swing.JFrame;
 
 import org.json.simple.*;
 
+import com.esotericsoftware.jsonbeans.Json;
+import com.esotericsoftware.jsonbeans.OutputType;
+
+import game.Game;
 import game.Location;
 import game.Player;
 import game.Direction;
@@ -23,9 +32,47 @@ import game.objects.interactiveObjects.*;
  */
 public class GameToJson {
 
+	//PRASHANT SAVING 
+	public static void saveGame(JFrame parentFrame, Game game) {
+	    Json json = new Json();
+	    json.setOutputType(OutputType.json);
+	    //String text = json.prettyPrint(game);// json.toJson(game);
+	    //System.out.println(text);
+	    
+	    FileDialog fDialog = new FileDialog(parentFrame, "Save Game as file..", FileDialog.SAVE);
+        fDialog.setDirectory(".");
+        fDialog.setFile("game.json");
+        fDialog.setVisible(true);
+        if(fDialog.getFile() == null) {
+        	return;
+        }
+        String path = fDialog.getDirectory() + fDialog.getFile();
+        System.out.println(path);
+        File f = new File(path);
+        try {
+			f.createNewFile();
+		} catch (IOException e) {
+		}
+        
+        try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+			bw.write(json.toJson(game));
+			bw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
+	public static void main(String[] args) {
+		saveGame(null, Game.createTestMap());
+	}
+	
+	/*
 	//@SuppressWarnings("unchecked")
 	public static void savePlayer(Player p) throws IOException, InvalidSaveException{
+		
+		
 		JSONObject jsonobj = new JSONObject();
 		
 		String name = p.getName();
@@ -61,6 +108,6 @@ public class GameToJson {
 			e.printStackTrace();
 			System.out.println("File not saved");
 		} 
-	}
+	}*/
 	
 }
--- src/Storage/JsonToGame.java
+++ src/Storage/JsonToGame.java
@@ -1,18 +1,32 @@
 package Storage;
 
+import java.awt.FileDialog;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+
+
+
+import java.util.Scanner;
+
+import javax.swing.JFrame;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ContainerFactory;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import com.esotericsoftware.jsonbeans.Json;
+import com.esotericsoftware.jsonbeans.OutputType;
+
+import game.Game;
 import game.Location;
 import game.Player;
 import game.avatar.Avatar;
@@ -20,7 +34,30 @@ import game.objects.interactiveObjects.*;
 
 
 public class JsonToGame {
-	@SuppressWarnings("unchecked")
+	
+	//PRASHANT LOADING 
+	public static Game loadGame(JFrame parentFrame) {
+	    Json json = new Json();
+	    json.setOutputType(OutputType.json);
+	    
+	    FileDialog fDialog = new FileDialog(parentFrame, "Load Game as file..", FileDialog.LOAD);
+        fDialog.setDirectory(".");
+        fDialog.setFile("game.json");
+        fDialog.setVisible(true);
+        if(fDialog.getFile() == null) {
+        	return null;
+        }
+        String path = fDialog.getDirectory() + fDialog.getFile();
+
+		Game g = json.fromJson(Game.class, new File(path));
+		System.out.println(g.toString());
+		return g;
+	}	
+	
+	public static void main(String[] args) {
+		loadGame(null);
+	}
+	/*@SuppressWarnings("unchecked")
 	public static Player loadPlayer(){
 		Player p = new Player();
 		JSONParser jp = new JSONParser();
@@ -40,7 +77,7 @@ public class JsonToGame {
 		}
 		return p;
 	}
-	
+	*/
 }
 	
 	
