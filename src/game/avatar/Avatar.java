d78ad40433dbe8ebe11833df7e7798b1c747b7cf
Prashant B <prashant_bhikhu@hotmail.com>
2015-10-13 02:28:00 +1300

    Half Arsed Comments for Avatar - Will fix later

--- src/game/avatar/Avatar.java
+++ src/game/avatar/Avatar.java
@@ -29,29 +29,39 @@ public class Avatar implements Serializable {
 	
 	private static File avatarPath = new File("./sprites/avatars/"); // The file path that locates the avatar sprites
 
-	// Although redunant due to 'evolution1_name', it is keep in the code for compatibility
-	private final String avatarName;
-	private final ImageIcon displayPic;
-	private ArrayList<Evolution> evolutions = new ArrayList<Evolution>();
+	private final String avatarName; // Although redunant due to 'evolution1_name', it is keep in the code for compatibility
+	private final ImageIcon displayPic; // The picture used when a player chooses a character
+	private ArrayList<Evolution> evolutions = new ArrayList<Evolution>(); // The list of evolutions for a particualr avatar
 
-
+	/**
+	 * Gets All Avatars dynamically from the Avatar resources folder, and parses images into
+	 * categories of evolutions for easier use.
+	 * 
+	 * @return A list of avatars retrieved from the file.
+	 * @throws FileNotFoundException Thrown if the files for a particular evolution are not found.
+	 */
 	public static List<Avatar> getAllAvatars() throws FileNotFoundException
 	{
+		// Initialize the avatars list for returning
 		List<Avatar> avatars;
 
+		// Checks if the Avatar Path even exist
 		if(avatarPath.exists() && avatarPath.isDirectory()) {
 			avatars = new ArrayList<Avatar>();
 		} else {
 			throw new FileNotFoundException("Sprite Avatar Folder Does not exist or cannot be found.");
 		}
 
+		// Get all Avatar directories
 		File[] fList = avatarPath.listFiles();
 
+		// Iterate through all Avatar and create the evolutions for the specific avatar
 		for (File file : fList) {
+			// Only enter a file if it is a directory
 			if (file.isDirectory()) {	
+				// Makes the directory always changes
 				File[] directories = null;
 				
-				//System.out.println(checkImagesExist(file.getAbsolutePath(), 1));
 				if(!checkImagesExist(file.getAbsolutePath(), 1)) {
 					throw new FileNotFoundException("Sprite Avatar Folder, in \""+file.getAbsolutePath()+"\"  are not Structured Correctly.");
 				}
@@ -134,6 +144,15 @@ public class Avatar implements Serializable {
 
 	}
 	
+	/**
+	 * Checks if the correct images exist within the specified directory for that evolution level.
+	 * The reason why evolutionLevel needs to be specified is because the different evolutions
+	 * have different sprite requirements from each other.
+	 * 
+	 * @param directory The Directory which the specified evolution sprites are located in.
+	 * @param evolutionLevel The evolution level of the directory
+	 * @return
+	 */
 	private static boolean checkImagesExist(String directory, int evolutionLevel) {
 		if(evolutionLevel == 1) {
 			return new File(directory + "/dp.png").exists() 
@@ -168,7 +187,17 @@ public class Avatar implements Serializable {
 		}
 	}
 	
-	public Avatar(String avatarName, ImageIcon displayPic,
+	/**
+	 * Creates an Avatar object. Although not used publicly, it is used by the static method
+	 * to get all the Avatar images in a structured way.
+	 * 
+	 * @param avatarName Name of the Avatar
+	 * @param displayPic Display picture of Avatar
+	 * @param firstEvolution The first evolution of Avatar
+	 * @param secondEvolution The Second evolution of Avatar
+	 * @param thirdEvolution The Second evolution of Avatar
+	 */
+	private Avatar(String avatarName, ImageIcon displayPic,
 			Evolution firstEvolution, Evolution secondEvolution, Evolution thirdEvolution) {
 		this.avatarName = avatarName;
 		this.displayPic = displayPic;
@@ -186,8 +215,9 @@ public class Avatar implements Serializable {
 		this.displayPic = null;
 	}	
 
-	
-	
+	/**
+	 * @return The name of the Avatar
+	 */
 	public String getName() {
 		return avatarName;
 	}
@@ -218,6 +248,12 @@ public class Avatar implements Serializable {
 		return true;
 	}
 
+	/**
+	 * 
+	 * 
+	 * @param playerEvolutionLevel The Evolution level that the 
+	 * @return
+	 */
 	public Evolution getCurrentEvolution(int playerEvolutionLevel) {
 		if(playerEvolutionLevel < 1 || playerEvolutionLevel > 3) {
 			throw new IllegalArgumentException("The Evolution Level of a Player must be between 1-3.");
@@ -230,6 +266,13 @@ public class Avatar implements Serializable {
 			throw new IllegalArgumentException("The Evolution Level of a Player must be between 1-3.");
 		}
 		return evolutions.get(playerEvolutionLevel);
+	}
+	
+	/**
+	 * @return the character's display picture
+	 */
+	public ImageIcon getDisplayPic() {
+		return displayPic;
 	}
 	
 	/**
@@ -247,9 +290,4 @@ public class Avatar implements Serializable {
 			throw new RuntimeException(e);
 		}
 	}
-
-	public ImageIcon getDisplayPic() {
-		return displayPic;
-	}
-
 }
