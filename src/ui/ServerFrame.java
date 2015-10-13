591868b113627fdb61cc1298e3fbc574d01aaa13
Prashant B <prashant_bhikhu@hotmail.com>
2015-10-14 09:08:11 +1300

    Added Comments

--- src/ui/ServerFrame.java
+++ src/ui/ServerFrame.java
@@ -26,7 +26,11 @@ import storage.SaverLoader;
 import storage.XMLToGame;
 import network.GameServer;
 /**
- * create this frame for showing information of server
+ * This class is the 'view' of the server. It will show various information of the server
+ * such as incoming and outgoing packets, who's connected etc. It also allows us to manage
+ * the server (via server on-off toggle) and allows us to load or save the game.
+ * 
+ * @author Prashant Bhikhu
  */
 @SuppressWarnings("serial")
 public class ServerFrame extends JFrame implements ActionListener, WindowListener {
@@ -34,8 +38,7 @@ public class ServerFrame extends JFrame implements ActionListener, WindowListene
 	private static final int FRAME_WIDTH = 600;
 	private static final int FRAME_HEIGHT = 300;
 	
-	
-	
+	// The Menu and Menu Items attached to the Server Frame 
 	private JMenuBar menuBar = new JMenuBar();
 	
 	JMenu gameMenu = new JMenu("Server");
@@ -44,7 +47,7 @@ public class ServerFrame extends JFrame implements ActionListener, WindowListene
 	JMenuItem exit = new JMenuItem("Exit");
 	
 	JMenu SaveLoadMenu = new JMenu("Save");
-	JMenuItem savePlayer = new JMenuItem("Save Game World");
+	JMenuItem saveGame = new JMenuItem("Save Game World");
 	
 	JMenu clearServerLogMenu = new JMenu("Clear Server Log...");
 	JMenuItem clearServerLog = new JMenuItem("Clear");
@@ -53,6 +56,8 @@ public class ServerFrame extends JFrame implements ActionListener, WindowListene
 	private JScrollPane consoleScrollPane;
 	
 	private GameServer gameServer;
+	
+	// For XML Saving (although never used)
 	private GameToXML saver;
 	private XMLToGame loader;
 	private SaverLoader sl;
@@ -73,16 +78,17 @@ public class ServerFrame extends JFrame implements ActionListener, WindowListene
 		
 		menuBar.add(SaveLoadMenu);
 
-		SaveLoadMenu.add(savePlayer);
+		SaveLoadMenu.add(saveGame);
 		
 		menuBar.add(clearServerLogMenu);
 		
 		clearServerLogMenu.add(clearServerLog);
 				
+		// Add Listners to all Menu Items
 		connect.addActionListener(this);
 		disconnect.addActionListener(this);
 		exit.addActionListener(this);
-		savePlayer.addActionListener(this);
+		saveGame.addActionListener(this);
 		clearServerLog.addActionListener(this);
 		
 		// Sets the Console properties so that it cannot be editted (only for display), and auto scrolls
@@ -97,25 +103,35 @@ public class ServerFrame extends JFrame implements ActionListener, WindowListene
 		// Finally show frame
 		setVisible(true);
 		
+		// Finally start the server on frame load
 		connect();
 	}
 
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
+		/* The Frame only listens for JMenuItems, hence why we can make the assumption
+		 * that every source is going to be a MenuItem. No additional pre-condition if 
+		 * statement required. 
+		 */
 		JMenuItem menuItem = (JMenuItem) arg0.getSource();
-		
+
+		// Determine what menu item was pressed
 		if(menuItem == connect) {
 			connect();
 		} else if(menuItem == disconnect) {
+			// reset the menu item enabled variables based on the change in server state.
 			connect.setEnabled(true);
 			disconnect.setEnabled(false);
-			savePlayer.setEnabled(false);
+			saveGame.setEnabled(false);
+			// Disconnects the server
 			gameServer.disconnect();
 			gameServer = null;
 		} else if(menuItem == exit) {
+			// Closes window and safely disconnects server
 			windowClosing(null);
-		} else if (menuItem == savePlayer) {
+		} else if (menuItem == saveGame) {
+			// Save the current state of the game
 			GameToJson.saveGame(this, gameServer.getGame());
 			/*sl = new SaverLoader();
 			try {
@@ -125,21 +141,30 @@ public class ServerFrame extends JFrame implements ActionListener, WindowListene
 				e.printStackTrace();
 			}*/
 		} else if (menuItem == clearServerLog) {
+			// Clear the server log (if its gets too spammy)
 			console.setText("");
 		}
 	}
 	
+	/**
+	 * Attempts to start a server connection
+	 */
 	public void connect() {
 		try {
 			gameServer = new GameServer(this);
+			// reset the menu item enabled variables based on the change in server state.
 			connect.setEnabled(false);
 			disconnect.setEnabled(true);
-			savePlayer.setEnabled(true);
+			saveGame.setEnabled(true);
 		} catch (IOException e) {
+			// Cannot start the server (likely problem is that there is a server already running on the port)
+			
+			// Show an error to the user so they know the server is having issues
 			JOptionPane.showInternalMessageDialog(this, "Cannot connect", "Server Error", JOptionPane.ERROR_MESSAGE);
+			// reset the menu item enabled variables based on the change in server state.
 			connect.setEnabled(true);
 			disconnect.setEnabled(false);
-			savePlayer.setEnabled(false);
+			saveGame.setEnabled(false);
 			return;
 		}	
 		
@@ -149,7 +174,8 @@ public class ServerFrame extends JFrame implements ActionListener, WindowListene
 			// Checks if User wants to load Game from file
 			Game loadedGame = JsonToGame.loadGame(this);
 			String path = sl.loadPath();
-			// if the load is null, then the user canceled the load operation, do do anything.
+			// if game loaded is null, then the user canceled the load operation, do do anything otherwise
+			// if the game loaded is valid, set it as the global server game.
 			if(loadedGame == null) {
 				return;
 			} else {
@@ -159,21 +185,11 @@ public class ServerFrame extends JFrame implements ActionListener, WindowListene
 	}
 	
 	/**
-	 * write message ToConsole
-	 * @param message
+	 * Writes (or appends) a message to the in-frame console. (NOT the Java Console)
+	 * @param message the message that is going to be displayed in console
 	 */
 	public void writeToConsole(String message) {
 		console.append(message + "\n");
-	}
-
-
-	@Override
-	public void windowActivated(WindowEvent e) {	
-	}
-
-
-	@Override
-	public void windowClosed(WindowEvent e) {
 	}
 
 
@@ -185,6 +201,16 @@ public class ServerFrame extends JFrame implements ActionListener, WindowListene
 		this.dispose();
 	}
 
+	// UNUSED METHODS
+
+	@Override
+	public void windowActivated(WindowEvent e) {	
+	}
+
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+	}
 
 	@Override
 	public void windowDeactivated(WindowEvent e) {	
