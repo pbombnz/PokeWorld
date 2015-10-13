98d3349f93f4bceb08b4dcaf1b21f58df59132f4
Prashant B <prashant_bhikhu@hotmail.com>
2015-10-14 11:26:40 +1300

    Added Comments to Packet and ServerFrame

--- src/network/Packets.java
+++ src/network/Packets.java
@@ -16,6 +16,58 @@ import game.objects.interactiveObjects.Item;
 *
 */
 public class Packets {
+	
+	/**
+	 * USED: Client -> Server
+	 * 
+	 * This packet is used when the clients needs to verify if the user name 
+	 * they choose is unique 
+	 * 
+	 */
+	public static class ValidateNewPlayerUsername {
+		String name;
+	}
+	
+	/**
+	 * USED: Server -> Client
+	 * 
+	 * This packet is used when the clients needs to verify if the user name 
+	 * they choose is unique, this packet confirms whether it is or not to the
+	 * client who is awaiting for a responses
+	 * 
+	 */
+	public static class ValidateNewPlayerUsername_Response {
+		boolean valid;
+	}	
+		
+	/**
+	 * USED: Client -> Server 
+	 *
+	 * This packet is used when the client tells the server that it is ready
+	 * to choose a player
+	 */
+	public static class ClientOnChoosePlayer {
+	}
+
+	/**
+	 * USED: Server -> Client 
+	 *
+	 * This packet is used when the server responds to a specific client's packet
+	 * (ClientOnChoosePlayer) and sends over any saved players from a file. This is
+	 * so the client knows to pick a savedFilePlayer if there is any or whether to 
+	 * create a completely new player.
+	 */
+	public static class ClientOnChoosePlayer_Response {
+		ArrayList<Player> savedFilePlayers;
+	}
+	
+	/**
+	 * USED: Client -> Server 
+	 *
+	 * This packet is used when the client picks a client player from the saved
+	 * file. It gets handled by the server differently as it replaced the old
+	 * ID and name with the new name and IDs so the client can use it
+	 */
 	public static class ClientUseExistingPlayer {
 		int oldId;
 		int newId;
@@ -23,22 +75,6 @@ public class Packets {
 		String newName;
 	}
 	
-	public static class ValidateNewPlayerUsername {
-		String name;
-	}
-	public static class ValidateNewPlayerUsername_Response {
-		boolean valid;
-	}
-	
-	public static class ServerQuit {
-	}	
-		
-	public static class ClientOnChoosePlayer {
-	}
-	
-	public static class ClientOnChoosePlayer_Response {
-		ArrayList<Player> savedFilePlayers;
-	}
 	/**
 	 * USED: Client -> Server
 	 *
@@ -48,7 +84,9 @@ public class Packets {
 	 * NOTE: Issues will occur when reading the Avatar object of the Player.
 	 * This is because for some reason, when passing an ImageIcon object
 	 * over the network, it adds the prefix "file:" which causes all ImageIcon
-	 * Images to be unuseable. 
+	 * Images to be unusable. 
+	 * 
+	 * UPDATE NOTE: I don't think its doing the above anymore
 	 * 
 	 */
 	public static class ClientNewPlayer {
@@ -68,44 +106,64 @@ public class Packets {
 		byte[] gameByteArray;
 	}
 	
+	/**
+	 * USED: Client -> Server, then Server -> All Client
+	 *
+	 * This packet is used when the local player is updated. This sends
+	 * a message to the server and passes through to other clients to
+	 * update other player's info.
+	 */
+	public static class PlayerUpdate {
+		int id;
+		int newPlayerLevel;
+		int newHealth;
+		int newAttack;
+	}
 	
 	/**
 	 * USED: Client -> Server, then Server -> All Clients 
 	 * 
-	 * A packet used when there is any change in variables for a player
-	 * (including move).
-	 *
+	 * A packet used when there is any change in location or direction for a player 
 	 */
 	public static class PlayerUpdateLocationAndDirection {
 		int id;
 		Location newLocation;
 		Direction newDirection;
 	}
-	
+
+	/**
+	 * USED: Client -> Server, then Server -> All Clients 
+	 * 
+	 * A packet used when a player picks up a particular item, the location, item and 
+	 * the player that picked it up (the player ID) need to be sent to everyone so
+	 * all client's game, see the object disappear, and within the game object, added
+	 * to the player's inventory.
+	 */
 	public static class PlayerPickUpItem {
 		int id;
 		Item item;
 		Location location;
 	}
 	
+	/**
+	 * USED: Client -> Server, then Server -> All Clients 
+	 * 
+	 * A packet used when a player dropped a particular item, the location dropped on, item and 
+	 * the player that dropped it (the player ID) need to be sent to everyone so
+	 * all client's game, see the object appear on the map and within the gae object, removed
+	 * from the player's inventory.
+	 */
 	public static class PlayerDropItem {
 		int id;
 		Item item;
 		Location location;
 	}
 	
-	public static class PlayerUpdate {
-		int id;
-		int newPlayerLevel;
-		int newHealth;
-		int newAttack;
-	}	
 	
 	/**
 	 * USED: Client -> Server, then Server -> All Clients 
 	 * 
-	 * A packet used when there is  a player sends a message for other players to view.
-	 *
+	 * A packet used when there is a player sends a message for other players to view.
 	 */
 	public static class ClientMessage {
 		String playerName;
@@ -115,12 +173,18 @@ public class Packets {
 	/**
 	 * USED: Client -> Server, then Server -> All Clients 
 	 * 
-	 * A packet that sends both ways, initially from client to server,
-	 * then the server passes it to the all clients. Used when there is
-	 * any change in variables for a player (including move).
-	 *
+	 * Used when a player leaves the server. This notifies other client games to remove
+	 * the player from the game board (and essentially the whole game).
 	 */
 	public static class ClientQuit {
 		int id;
+	}
+	
+	/**
+	 * USED: Server -> Client
+	 * 
+	 * A packet that tells clients that the server has ended, hence end of gameplay.
+	 */
+	public static class ServerQuit {
 	}
 }
